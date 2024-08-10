b8905fd302e1f73f8ce5608484afe60a5452e52d
akshay c s <akshaycs0480@gmail.com>
2024-08-10 12:19:34 +0530

    project successfully completed

--- controllers/BillController.js
+++ controllers/BillController.js
@@ -1,7 +1,6 @@
 import Bill from "../models/BillItems.js";
 import InventoryItem from "../models/InventoryItems.js";
 
-
 export const createBill = async (req, res) => {
   try {
     const { items } = req.body;
@@ -17,7 +16,6 @@ export const createBill = async (req, res) => {
       await itemData.save();
     }
 
-
     const bill = new Bill({ ...req.body, totalAmount });
     await bill.save();
     res.status(201).json(bill);
@@ -26,28 +24,24 @@ export const createBill = async (req, res) => {
   }
 };
 
-
 export const getBills = async (req, res) => {
-    try {
-      const bills = await Bill.find().populate('items.itemId');
-      res.status(200).json(bills);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+  try {
+    const bills = await Bill.find().populate("items.itemId");
+    res.status(200).json(bills);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-  
-  export const getBill = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const bill = await Bill.findById(id).populate('items.itemId');
-      if (!bill) {
-        return res.status(404).json({ message: "Bill not found" });
-      }
-      res.status(200).json(bill);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+export const getBill = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bill = await Bill.findById(id).populate("items.itemId");
+    if (!bill) {
+      return res.status(404).json({ message: "Bill not found" });
     }
-  };
-
-  
+    res.status(200).json(bill);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- models/BillItems.js
+++ models/BillItems.js
@@ -1,26 +1,19 @@
-// // import mongoose from "mongoose";
-
-// // const BillItemSchema = new mongoose.Schema({
-// //   item: { type: mongoose.Schema.Types.ObjectId, ref: "Item", required: true },
-// //   quantity: { type: Number, required: true },
-// // });
-
-// // const BillItem = mongoose.model("BillItem", BillItemSchema);
-
-// // export default BillItem;
-
-
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const billSchema = new mongoose.Schema({
   date: { type: Date, required: true },
-  items: [{
-    itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'InventoryItem', required: true },
-    quantity: { type: Number, required: true }
-  }],
-  totalAmount: { type: Number, required: true }
+  items: [
+    {
+      itemId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "InventoryItem",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalAmount: { type: Number, required: true },
 });
 
-const Bill = mongoose.model('Bill', billSchema);
+const Bill = mongoose.model("Bill", billSchema);
 export default Bill;
